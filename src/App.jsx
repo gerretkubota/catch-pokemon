c4b59c844a5987a79443541f47bce05e73db3035
gerretkubota <gerretkubota@gmail.com>
2020-01-04 22:21:39 -0800

    mis-commit from directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,18 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import axios from 'axios';
-import Pokemon from './components/Pokemon/Pokemon.jsx';
-import PokemonList from './components/PokemonList/PokemonList.jsx';
-import Loading from './components/Loading/Loading.jsx';
+import React, { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
+import PokemonInfo from './components/PokemonInfo/PokemonInfo.jsx';
 
 import './main.css';
 
-const App = () => {
-  const pokemonURL = 'https://pokeapi.co/api/v2';
-  const [wildPokemon, setWildPokemon] = useState();
-  const [caughtPokemon, setCaughtPokemon] = useState([]);
-  const [totalPokemon, setTotalPokemon] = useState(-1);
-
-  useEffect(() => {
-    grabAllPokemon();
-  }, []);
-
-  useEffect(() => {
-    if (totalPokemon > 0) {
-      encounterPokemon();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [totalPokemon]);
-
-  const calcRandomPokemon = () => Math.floor(Math.random() * totalPokemon) + 1;
-
-  const grabAllPokemon = () => {
-    let cancel;
-    axios
-      .get(`${pokemonURL}/pokedex/national/`, {
-        cancelToken: new axios.CancelToken(c => (cancel = c)),
-      })
-      .then(res => setTotalPokemon(res.data.pokemon_entries.length))
-      .catch(err => alert(err));
-
-    return () => cancel();
-  };
-
-  const encounterPokemon = () => {
-    let cancel;
-    axios
-      .get(`${pokemonURL}/pokemon/${calcRandomPokemon()}`, {
-        cancelToken: new axios.CancelToken(c => (cancel = c)),
-      })
-      .then(res =>
-        setWildPokemon({
-          name: res.data.name,
-          image: res.data.sprites.front_default,
-          id: res.data.id,
-        })
-      )
-      .catch(err => alert(err));
-    return () => cancel();
-  };
-
-  const catchPokemon = () => {
-    setCaughtPokemon(state => {
-      const pokemonExists = state.findIndex(
-        pokemon => wildPokemon.id === pokemon.id
-      );
-      if (pokemonExists < 0) {
-        state = [...state, wildPokemon];
-        state.sort((a, b) => a.id - b.id);
-      }
-      return state;
-    });
-    encounterPokemon();
-  };
-
-  const releasePokemon = (e, index) => {
-    setCaughtPokemon(state => {
-      state.splice(index, 1);
-      return [...state];
-    });
-  };
-
-  return (
-    <div className="app-container">
-      <div className="app-header">Catch a Pokemon!</div>
-      {wildPokemon && totalPokemon ? (
-        <Pokemon
-          name={wildPokemon.name}
-          image={wildPokemon.image}
-          catchPokemon={catchPokemon}
-        />
-      ) : (
-        <Loading />
-      )}
-      <PokemonList
-        caughtPokemon={caughtPokemon}
-        releasePokemon={releasePokemon}
-      />
-    </div>
-  );
-};
+const App = () => (
+  <div className="app-container">
+    <div className="app-header">Catch a Pokemon!</div>
+    <Switch>
+      <Route exact path="/" component={Dashboard} />
+      <Route path="/pokemon/:pokemonId" component={PokemonInfo} />
+    </Switch>
+  </div>
+);
 
 export default App;
